e9f8d351183af8026228aa198f36407c0c136016
Rico Tiongson <thericotiongson@gmail.com>
2016-01-20 11:48:34 +0800

    Added local shifts. Removed friend tag for operator overloads.

--- double_int.hpp
+++ double_int.hpp
@@ -15,15 +15,14 @@ struct double_int {
 	T2 second;
 
 	// constructor
+	double_int(): T1(), T2() {}
 	double_int(const T1& first, const T2& second):
 		first(first),
 		second(second) {}
-
 	template<class U1, class U2>
 	double_int(const double_int<U1, U2>& d):
 		first(T1(d.first)),
 		second(T2(d.second)) {}
-
 	double_int(const T2& x):
 		first(0),
 		second(x) {}
@@ -31,12 +30,13 @@ struct double_int {
 	// binary bit operations
 	#undef binary_operation
 	#define binary_operation(op) \
-	inline friend double_int<T1, T2> operator op (const double_int<T1, T2>& a, const double_int<T1, T2>& b) { \
-		return double_int<T1, T2>(a.first op b.first, a.second op b.second); \
+	template<class U1, class U2> \
+	inline friend double_int operator op (const double_int<U1, U2>& b) const { \
+		return double_int<T1, T2>(first op b.first, second op b.second); \
 	} \
-	inline friend double_int<T1, T2> operator op (const double_int<T1, T2>& a, const T2& b) { \
-		return double_int<T1, T2>(a.first, a.second op b); \
-	}
+	inline friend double_int operator op (const T2& b) const { \
+		return double_int<T1, T2>(first, second op b); \
+	} \
 	binary_operation(|)
 	binary_operation(&)
 	binary_operation(^)
@@ -44,12 +44,13 @@ struct double_int {
 
 	// binary bit reassign operations
 	#define binary_operation(op) \
-	inline friend double_int<T1, T2>& operator op (const double_int<T1, T2>& a, const double_int<T1, T2>& b) { \
-		a.first op b.first; a.second op b.second; return a; \
+	template<class U1, class U2> \
+	inline friend double_int& operator op (const double_int<U1, U2>& b) const { \
+		first op b.first; second op b.second; return *this; \
 	} \
-	inline friend double_int<T1, T2>& operator op (const double_int<T1, T2>& a, const T2& b) { \
-		a.second op b; return a; \
-	}
+	inline friend double_int& operator op (const T2& b) const { \
+		second op b; return *this; \
+	} \
 	binary_operation(|=)
 	binary_operation(&=)
 	binary_operation(^=)
@@ -80,39 +81,51 @@ struct double_int {
 	}
 
 	// shift operations
-	inline friend double_int<T1, T2> operator << (const double_int<T1, T2>& a, int shift) {
+	inline double_int operator << (int shift) const {
 		int bits = sizeof(T2) << 3;
-		return double_int<T1, T2>((a.first << shift) | T1(a.second >> (bits - shift)), a.second << shift);
+		return double_int((first << shift) | T1(second >> (bits - shift)), second << shift);
 	}
-
-	inline friend double_int<T1, T2> operator >> (const double_int<T1, T2>& a, int shift) {
+	inline double_int& operator << (int shift) {
 		int bits = sizeof(T2) << 3;
-		return double_int<T1, T2>(a.first >> shift, (a.second >> shift) | T2((shift == (sizeof(T1) << 3)) ? a.first : a.first & ((T1(1) << shift) - 1)));
+		first = (first << shift) | T1(second >> (bits - shift));
+		second <<= shift;
+		return *this;
+	}
+	inline double_int operator >> (int shift) const {
+		int bits = sizeof(T2) << 3;
+		return double_int(first >> shift, (second >> shift) | T2(first & ((T1(1) << shift) - 1));
+	}
+	inline double_int& operator >> (int shift) {
+		int bits = sizeof(T2) << 3;
+		second = (second >> shift) | T2(first & ((T1(1) << shift) - 1);
+		first >>= shift;
+		return *this;
 	}
 
 	// overload if(*this) and while (*this)
 	operator bool() const {
 		return bool(first) || bool(second);
 	}
-
 	// overload if(!*this)
 	inline bool operator !() const {
 		return !bool(*this);
 	}
-
 	// overload unary flip
 	inline double_int<T1, T2> operator~() const {
 		return double_int<T1, T2>(~first, ~second);
 	}
-
-	// only supports integral double_int
-	
+	// TODO: separate integral double_int and recursive double_int
 	inline friend ostream& operator << (ostream& out, const double_int<T1, T2>& d) {
 		return out << bitset<(sizeof(T1) << 3)>(d.first) << ' ' << bitset<(sizeof(T2) << 3)>(d.second);
 	}
-
-	inline bool operator < (const double_int<T1, T2>& b) const {
+	// TODO: arithmetic operations
+	template<class U1, class U2>
+	inline bool operator < (const double_int<U1, U2>& b) const {
 		return first < b.first || (first == b.first && second < b.second);
+	}
+	template<class U1, class U2>
+	inline bool operator == (const double_int<U1, U2>& b) const {
+
 	}
 
 };
